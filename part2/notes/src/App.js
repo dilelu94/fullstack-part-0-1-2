de29daff77ecf8a47ca5896309b9f752abc451bd
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-05 21:20:56 -0300

    Exercises 2.11.-2.14.

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,12 +1,28 @@
-import { useState } from "react"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from "./Components/Note"
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+/* npx json-server --port 3001 --watch db.json */
+
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   ) 
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+/*   console.log('render', notes.length, 'notes') */
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
