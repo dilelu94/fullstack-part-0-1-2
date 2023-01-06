1f15817670a28172bf9ec98a36b0d394ebb7b38d
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-06 01:00:51 -0300

    2.11: The Phonebook Step6

--- part2/Phonebook/src/App.js
+++ part2/Phonebook/src/App.js
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import SubmitForm from './Components/SubmitForm';
 import PersonList from './Components/PersonList';
 import Filter from './Components/Filter';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newId, setNewId] = useState(5)
   const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
--- part2/Phonebook/src/index.js
+++ part2/Phonebook/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+/* npm install -g json-server */
+/* npm install axios */
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
