0bbfff7e783db284a12c6500e07b25a926884e7f
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-13 23:34:19 -0300

    REST, Fetching a single resource

---
+++ excersicePractice/simpleWebServer/index.js
@@ -0,0 +1,56 @@
+/* npm install --save-dev nodemon */
+/* Set-ExecutionPolicy Unrestricted */
+/* node_modules/.bin/nodemon index.js */
+
+/* { name: 'John', age: 30 } object */
+/* {"name":"John","age":30} json, string */
+
+const { response, request } = require('express')
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2022-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2022-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true
+    }
+]
+
+/* Fetching a single resource */
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
