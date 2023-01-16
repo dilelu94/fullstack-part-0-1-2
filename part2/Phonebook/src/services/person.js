a24b98b1180f7f43aeb0cceb7e96023dd9218ad7
dilelu94 <diego.leonel.luque@gmail.com>
2023-01-16 20:18:04 -0300

    3.11 phonebook full stack

--- part2/Phonebook/src/services/person.js
+++ part2/Phonebook/src/services/person.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://phonebookbackendluque.fly.dev/api/persons'
+const baseUrl = 'api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
